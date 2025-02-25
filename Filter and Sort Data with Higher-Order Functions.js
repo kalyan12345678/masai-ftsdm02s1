9f7ea307a92e50bd1334fba42576d930594af3f8
kalyan alladi <55277793+kalyan12345678@users.noreply.github.com>
2025-02-26 05:14:03 +0530

    Add files via upload

---
+++ Filter and Sort Data with Higher-Order Functions.js
@@ -0,0 +1,21 @@
+let arr= [ { "name": "Alice", "marks": 58 },
+  { "name": "Bob", "marks": 85 },
+  { "name": "Charlie", "marks": 92 },
+  { "name": "David", "marks": 45 },
+  { "name": "Emma", "marks": 76 },
+  { "name": "Frank", "marks": 63 },
+  { "name": "Grace", "marks": 89 },
+  { "name": "Hannah", "marks": 95 },
+  { "name": "Ian", "marks": 54 },
+  { "name": "Jack", "marks": 79 },
+  { "name": "Kate", "marks": 67 },
+  { "name": "Leo", "marks": 88 },
+  { "name": "Mia", "marks": 91 },
+  { "name": "Nathan", "marks": 72 },
+  { "name": "Olivia", "marks": 82 }
+]
+let narr=arr.filter((ele,i,arr)=>{
+    return ele.marks>60
+}).sort((a, b) => {
+    return b.marks - a.marks});
+console.log(narr)
